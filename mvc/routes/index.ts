675fb5065835963210e2bd3f9c54f9b2a214a526
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-04-18 12:14:55 +0200

    adding default route

---
+++ mvc/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+const router = Router();
+
+router.get("/", (_req: Request, res: Response, _next: NextFunction) =>
+  res.sendStatus(200)
+);
+
+export default router;
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import app from "./express";
 import http from "http";
+import routes from "./mvc/routes";
 import { initSocketIO } from "./socketio";
 
 const PORT: number = Number(process.env.PORT) || 8000;
@@ -8,6 +9,9 @@ const server = http.createServer(app);
 
 // initiate socketio
 initSocketIO(server);
+
+// routes
+app.use("/api/", routes);
 
 server.listen(PORT, () => {
   console.log("Server running on port:", PORT);
