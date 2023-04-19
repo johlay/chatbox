38e9f2249cbfc5c9155a3fabbdb43e9c32487bc6
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-04-19 15:34:30 +0200

    renamed interface

--- db/models/user_model.ts
+++ db/models/user_model.ts
@@ -1,19 +1,19 @@
 import { Schema, model, connect } from "mongoose";
 
-interface IUserSchema {
+export interface UserSchema {
   first_name: string;
   last_name: string;
   email: string;
   password: string;
 }
 
-const userSchema = new Schema<IUserSchema>({
+const userSchema = new Schema<UserSchema>({
   first_name: { type: String, required: true, trim: true },
   last_name: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true, trim: true },
 });
 
-const User = model<IUserSchema>("User", userSchema);
+const User = model<UserSchema>("User", userSchema);
 
 export default User;
