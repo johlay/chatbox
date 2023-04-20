324d5ed3bd8042de8248e6de1b672e2deafc98da
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-04-20 16:25:21 +0200

    adding header

---
+++ client/src/components/Drawer.tsx
@@ -0,0 +1,47 @@
+import { NavItem } from "./Header";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import MuiDrawer, { DrawerProps } from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+
+type MobileDrawerProps = {
+  MobileProps: {
+    heading: string;
+    items: NavItem[];
+    onClick: () => void;
+  };
+};
+
+export const Drawer = (props: DrawerProps & MobileDrawerProps) => {
+  const { children, MobileProps, ...rest } = props;
+
+  const mobileDrawer = (
+    <Box onClick={MobileProps.onClick} sx={{ textAlign: "center" }}>
+      <Typography color={"#fff"} variant="h6" sx={{ my: 2 }}>
+        {MobileProps.heading}
+      </Typography>
+      <Divider sx={{ bgcolor: "#fff" }} />
+      <List>
+        {MobileProps.items.map((item: NavItem) => (
+          <ListItem key={item.text.toLowerCase()} disablePadding>
+            <ListItemButton href={item.url} sx={{ textAlign: "center" }}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <MuiDrawer {...rest}>
+      {children}
+      {mobileDrawer}
+    </MuiDrawer>
+  );
+};
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,108 @@
+import { Drawer } from "./Drawer";
+import { drawer as drawerVariables } from "./Variables";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+
+type HeaderProps = {
+  window?: () => Window;
+};
+
+type Url = "/" | "/register" | "/login";
+
+export type NavItem = {
+  url: Url;
+  text: string;
+};
+
+const navItems: NavItem[] = [
+  { url: "/", text: "Home" },
+  { url: "/register", text: "Register" },
+  { url: "/login", text: "Login" },
+];
+
+const Header = (props: HeaderProps) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="static" component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Helpchat
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item.text.toLocaleLowerCase()}
+                href={item.url}
+                sx={{ color: "#fff" }}
+              >
+                {item.text}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          MobileProps={{
+            onClick: handleDrawerToggle,
+            items: navItems,
+            heading: "Helpchat",
+          }}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          PaperProps={{
+            sx: {
+              backgroundColor: "primary.main",
+              color: "#fff",
+            },
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerVariables.width,
+            },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
