b9d3abc25bee338ffd99e6edd2b0eda2a272d917
Johnny Lay <86290365+johlay@users.noreply.github.com>
2023-04-26 15:19:09 +0200

    removing unused import

--- client/src/theme/Theme.tsx
+++ client/src/theme/Theme.tsx
@@ -1,4 +1,4 @@
-import { blueGrey, grey } from "@mui/material/colors";
+import { blueGrey } from "@mui/material/colors";
 import createTheme from "@mui/material/styles/createTheme";
 
 export const theme = createTheme({
